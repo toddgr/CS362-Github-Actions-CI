3e4ffcdcd0070499fe580380c86ec81f68f9fdb2
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 17:12:33 -0700

    added tests for add and sub

--- test.py
+++ test.py
@@ -11,9 +11,13 @@ class TestCase(unittest.TestCase):
 
     def test_add_1(self):
         self.assertEqual(example.add(1, 2), 3)
+        self.assertEqual(example.add(5, 2), 7)
+        self.assertEqual(example.add(13, 5), 18)
 
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
+        self.assertEqual(example.subtract(13, 7), 6)
+        self.assertEqual(example.subtract(20, 5), 15)
 
     def test_multiply_1(self):
         self.assertEqual(example.multiply(3, 4), 12)
