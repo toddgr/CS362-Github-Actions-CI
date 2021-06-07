9f943303f397319ce4ad06cb57814b711adf447e
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 17:14:49 -0700

    added tests for mult and div

--- test.py
+++ test.py
@@ -21,9 +21,13 @@ class TestCase(unittest.TestCase):
 
     def test_multiply_1(self):
         self.assertEqual(example.multiply(3, 4), 12)
+        self.assertEqual(example.multiply(20, 5), 100)
+        self.assertEqual(example.multiply(9, 6), 54)
 
     def test_divide_1(self):
         self.assertEqual(example.divide(12, 3), 4)
+        self.assertEqual(example.divide(45, 5), 9)
+        self.assertEqual(example.divide(400, 100), 4)
 
 
 if __name__ == '__main__':
